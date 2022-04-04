aeebda2028a2af2217a6f66250896cd472e4b0d8
DaviChalita <chalitameneguelli14@gmail.com>
2022-04-04 20:26:47 -0300

    fix scream trigger

--- Assets/Scripts/TriggerScream.cs
+++ Assets/Scripts/TriggerScream.cs
@@ -6,7 +6,9 @@ public class TriggerScream : MonoBehaviour
 {
     public AudioClip screamClip = default;
     public AudioSource audioSource;
+    public GameObject trigger;
     void OnTriggerEnter(Collider other){
         audioSource.PlayOneShot(screamClip);
+        trigger.SetActive(false);
     }
 }
